7c7db49526597e636ebcb704156735daf6bc07b3
kathiravankrays-at-383429423364 <padma220395@gmail.com>
2024-12-17 14:05:36 +0530

    Changes in Splash screen

--- lib/components/splash/spash.screen.dart
+++ lib/components/splash/spash.screen.dart
@@ -27,11 +27,53 @@ class _SplashScreenState extends State<SplashScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/app-logo.png',
-              fit: BoxFit.cover,
-              width: 300,
-            ),
+            /*const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text(
+                    "m",
+                    style: TextStyle(
+                      fontSize: 48, // Size: 48px
+                      fontFamily:
+                          "Lobster", // Font Family: Lobster
+                      fontWeight: FontWeight.w400, // Weight: 400
+                      color: Color(
+                          0xFF4ABFE2), // Color: rgb(74, 191, 226)
+                      height: 62 /
+                          48, // Line Height: 62px / 48px = ~1.29
+                    ),
+                  ),
+                  Text(
+                    "entorboosters",
+                    style: TextStyle(
+                      fontSize: 32, // Size: 32px
+                      fontWeight: FontWeight.w700, // Weight: 800
+                      fontFamily:
+                          "Epilogue", // Font Family: Epilogue, sans-serif
+                      color: Color(
+                          0xFF4ABFE2), // Color: rgb(74, 191, 226)
+                      height: 42 /
+                          32, // Line Height: 42px / 32px = ~1.31
+                    ),
+                  ),
+                  Text(
+                    ".",
+                    style: TextStyle(
+                      fontSize: 72, // Font size for the dot
+                      fontWeight: FontWeight
+                          .w800, // Match the same weight as text
+                      fontFamily: "Epilogue", // Font Family
+                      color: Color(0xFF4ABFE2), // Match the color
+                      height: 1, // Default height
+                    ),
+                  ),
+                ],
+              ),
+            ),*/
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -40,13 +82,13 @@ class _SplashScreenState extends State<SplashScreen> {
                   style: context.headlineSmall,
                 ),
                 Text(
-                  " Mentori",
+                  " Mentor Boosters",
                   style: context.displaySmall,
                 ),
               ],
             ),
             Text(
-              "PTz mentoring platform",
+              "Your Mentor partner in Progress",
               style: context.bodySmall,
             ),
           ],
