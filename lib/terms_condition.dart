0eaca0cdeeaf279648e85167f43e6f54bbd287cc
mac <mac@macs-MacBook-Pro-2.local>
2025-03-04 18:32:02 +0530

    terms & condition changes

--- lib/terms_condition.dart
+++ lib/terms_condition.dart
@@ -78,81 +78,69 @@ class TermsAndConditionsPage extends StatelessWidget {
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '2. Definitions',
+                    title: '2. User Eligibility',
                     content:
-                        ' • "Platform" refers to MentorBoosters.com and its associated mobile application. \n • "User" refers to any individual or entity that registers or uses the platform. \n • "Mentor" refers to individuals providing mentorship services.\n • "Mentee" refers to SMEs or individuals receiving mentorship services.\n • "Content" includes text, data, graphics, audio, video, and other materials shared on the platform.',
+                        ' • Users must be at least 18 years old to create an account. \n • By registering, users confirm that they have the legal ability to enter into agreements. \n • MentorBoosters.com reserves the right to suspend or terminate accounts if false information is provided.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '3. User Eligibility',
+                    title: '3. Registration and Account Management',
                     content:
-                        ' • Users must be at least 18 years old to create an account.\n • By registering, users confirm that they have the legal ability to enter into agreements.\n • MentorBoosters.com reserves the right to suspend or terminate accounts if false information is provided.',
+                        ' • Users must provide accurate and complete information during registration.\n • Users are responsible for maintaining the confidentiality of their login credentials. \n • MentorBoosters.com is not liable for unauthorized access resulting from user negligence.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '4. Registration and Account Management',
-                    content:
-                        ' • Users must provide accurate and complete information during registration.\n • Users are responsible for maintaining the confidentiality of their login credentials.\n • MentorBoosters.com is not liable for unauthorized access resulting from user negligence',
-                  ),
-                  const SizedBox(height: 40),
-                  _buildColumn(
-                    title: '5. Mentor and Mentee Responsibilities',
+                    title: '4. Mentor and Mentee Responsibilities',
                     content:
                         ' • Mentors must provide accurate and professional guidance within their expertise.\n • Mentees agree to actively engage in mentorship programs and respect scheduled sessions.\n • Users must not engage in fraudulent, misleading, or unethical behavior.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '6. Fees and Payments',
+                    title: '5. Fees and Payments',
                     content:
                         ' • Some services may require payment, which will be communicated to users.\n • Payment transactions will be processed securely through third-party payment providers.\n • MentorBoosters.com is not responsible for payment disputes between mentors and mentees.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '7. Content Ownership and Usage',
+                    title: '6. Content Ownership and Usage',
                     content:
-                        ' • Users retain ownership of the content they upload but grant MentorBoosters.com a nonexclusive, royalty-free license to use, \n modify, and display such content for platformrelated purposes.\n • Users must not post copyrighted or infringing content without appropriate rights ',
+                        ' • Users retain ownership of the content they upload but grant MentorBoosters.com a nonexclusive, royalty-free license to use,  \n modify, and display such content for platform-related purposes.\n • Users must not post copyrighted or infringing content without appropriate rights ',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: ' 8. Privacy and Data Protection',
+                    title: ' 7. Privacy and Data Protection',
                     content:
                         ' • MentorBoosters.com adheres to applicable data protection laws.\n • User data will not be sold or shared with third parties without consent, except asrequired by law.\n • Users agree to the collection and processing of their data as described in the Privacy Policy.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '9. Code of Conduct',
+                    title: '8. Code of Conduct',
                     content:
                         ' • Users must interact respectfully and professionally.\n • Hate speech, harassment, discrimination, and any illegal activities are strictly prohibited.\n • MentorBoosters.com reserves the right to suspend or remove users violating these terms.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '10. Limitation of Liability',
+                    title: '9. Limitation of Liability',
                     content:
                         ' • MentorBoosters.com is a facilitator and does not guarantee specific outcomes from mentorship.\n • The platform is not liable for any direct, indirect, or consequential damages arising from mentorship interactions.\n • Users agree to hold MentorBoosters.com harmless from claims resulting from their use of the platform.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '11. Termination and Suspension',
+                    title: '10. Termination and Suspension',
                     content:
                         ' • MentorBoosters.com reserves the right to suspend or terminate accounts for violations of these terms.\n • Users may terminate their accounts at any time, but fees paid are non-refundable.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '12. Amendments to Terms',
+                    title: '11. Amendments to Terms',
                     content:
                         ' • MentorBoosters.com may update these terms from time to time.\n • Users will be notified of significant changes and continued use of the platform constitutes acceptance.',
                   ),
                   const SizedBox(height: 40),
                   _buildColumn(
-                    title: '13. Governing Law and Dispute Resolution',
+                    title: '12. Contact Information',
                     content:
-                        ' • These terms shall be governed by the laws of [Insert Jurisdiction].\n • Any disputes shall first be attempted to be resolved through negotiation; if unsuccessful,they will be subject to arbitration or court \n  proceedings in the applicable jurisdiction.',
-                  ),
-                  const SizedBox(height: 40),
-                  _buildColumn(
-                    title: '14. Contact Information',
-                    content:
-                        'For inquiries regarding these Terms and Conditions, please contact us at [Insert Contact Email].\nBy using MentorBoosters.com, you acknowledge that you have read, understood, and agree to these Terms and Conditions.',
+                        'For inquiries regarding these Terms and Conditions, please contact us at Hello@mentorboosters.com.\nBy using MentorBoosters.com, you acknowledge that you have read, understood, and agree to these Terms and Conditions.',
                   ),
                 ],
               ),
